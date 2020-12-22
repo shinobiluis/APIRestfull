f803f28f8a64d265ef665d369fc9129d23dc32f8
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-12-21 22:34:36 -0600

    Implementando Modelos

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Ap\Category;
+use App\Seller;
+use App\Transaction;
 class Product extends Model
 {
-    //
+    const PRODUCTO_DISPONIBLE='disponible';
+    const PRODUCTO_NO_DISPONIBLE='no disponible';
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+
+    public function estaDisponible(){
+        return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
+
+    public function seller(){
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
